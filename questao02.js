07641d2e5cff1849c6f248ab722b53ad23f52c1f
jmarkus1 <164332458+jmarkus1@users.noreply.github.com>
2026-09-08 10:51:36 -0300

    questao02

---
+++ questao02.js
@@ -0,0 +1,9 @@
+const numeros = [10, 20, 30, 40, 50];
+
+// Extrai os três primeiros elementos do array para as variáveis a, b e c
+const [a, b, c] = numeros;
+
+// Exibe os valores extraídos no console
+console.log(a); // Saída: 10
+console.log(b); // Saída: 20
+console.log(c); // Saída: 30
