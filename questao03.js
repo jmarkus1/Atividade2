8d6f1719861d3c2a315919a4e3d2dce84d523325
jmarkus1 <164332458+jmarkus1@users.noreply.github.com>
2026-09-08 10:55:03 -0300

    questao03

---
+++ questao03.js
@@ -0,0 +1,8 @@
+const aluno = { nome: "Jefferson", idade: 21, curso: "Sistemas para Internet" };
+
+// Extrai as propriedades "nome" e "idade" criando variáveis de mesmo nome
+const { nome, idade } = aluno;
+
+// Exibe os valores no console
+console.log(nome);  // Saída: Jefferson
+console.log(idade); // Saída: 21
